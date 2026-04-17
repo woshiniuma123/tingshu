3893fe4152b6145d698864ec0190181d2bf3b7e6
zubenjian <2542112739@qq.com>
2026-04-17 18:44:32 +0800

    谷粒随享-day01-增加了获取全部分类功能

--- service/service-album/src/main/java/com/atguigu/tingshu/album/api/AlbumInfoApiController.java
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/api/AlbumInfoApiController.java
@@ -1,10 +1,16 @@
 package com.atguigu.tingshu.album.api;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.tingshu.album.service.AlbumInfoService;
+import com.atguigu.tingshu.common.result.Result;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "专辑管理")
 @RestController
@@ -12,9 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @SuppressWarnings({"all"})
 public class AlbumInfoApiController {
 
-	@Autowired
-	private AlbumInfoService albumInfoService;
-
+    @Autowired
+    private AlbumInfoService albumInfoService;
+    @Operation(summary = "获取1,2,3级分类")
+    @GetMapping("/category/getBaseCategoryList")
+    public Result<List<JSONObject>> getBaseCategoryList() {
+        List<JSONObject> list = albumInfoService.getBaseCategoryList();
+        return Result.ok(list);
+    }
 
 }
 
--- service/service-album/src/main/java/com/atguigu/tingshu/album/service/AlbumInfoService.java
+++ service/service-album/src/main/java/com/atguigu/tingshu/album/service/AlbumInfoService.java
@@ -1,9 +1,14 @@
 package com.atguigu.tingshu.album.service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.tingshu.model.album.AlbumInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 public interface AlbumInfoService extends IService<AlbumInfo> {
 
+
+    List<JSONObject> getBaseCategoryList();
 
 }
